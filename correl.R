185bd7e79b122ff5171efbc9689eb8c992fdfd41
bhatnash <bhatnash@users.noreply.github.com>
2018-10-29 13:08:12 -0400

    Update correl.R

--- correl.R
+++ correl.R
@@ -1,3 +1,4 @@
 data <- read.table("diabetic_data_cleaned.txt", header=TRUE, sep="\t")
 indx <- sapply(data, is.numeric)
-abs(cor(data[indx],use="pairwise.complete.obs"))
+cor_data<-abs(cor(data[indx],use="pairwise.complete.obs"))
+plot(cor_data)
